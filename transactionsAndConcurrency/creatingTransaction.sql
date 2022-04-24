82af6337d5f8b80037b3d575c5a673541a49f691
Uday <budayteja009@gmail.com>
2022-04-24 22:26:53 +0530

    Added codes

--- transactionsAndConcurrency/creatingTransaction.sql
+++ transactionsAndConcurrency/creatingTransaction.sql
@@ -1,11 +1,6 @@
 USE sql_store;
-
 START TRANSACTION;
-
-INSERT INTO orders(customer_id, order_Date, status) 
-VALUE (1, '2022-04-08', 1);
-
-INSERT INTO order_items 
-VALUES(LAST_INSERT_ID(), 1, 1, 1);
-
+UPDATE customers 
+SET points = points + 10
+WHERE customer_id = 1;
 COMMIT;
